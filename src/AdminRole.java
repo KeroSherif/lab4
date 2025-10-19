38bbae3e6aa3546449ae00d3c0f688e431b2d2d1
KeroSherif <kerosherif8@gmail.com>
2025-10-19 16:47:42 +0300

    Update AdminRole.java
    add validation

--- src/AdminRole.java
+++ src/AdminRole.java
@@ -6,9 +6,18 @@ public class AdminRole {
     }
 
     public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
-        EmployeeUser newUser = new EmployeeUser(employeeId, name, email, address, phoneNumber);
-        database.insertRecord(newUser);
+    if (employeeId == null || employeeId.trim().isEmpty()) {
+        System.err.println(" Employee ID cannot be empty.");
+        return;
     }
+ 
+    if (database.contains(employeeId)) {
+        System.err.println(" Employee with ID " + employeeId + " already exists.");
+        return;
+    }
+    EmployeeUser newUser = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+    database.insertRecord(newUser);
+}
 
     public EmployeeUser[] getListOfEmployees() {
         return database.returnAllRecords().toArray(new EmployeeUser[0]);
