744c8b92402c07de96c283f5878534321655bb67
mohamed alzagh <164375339+nuggut@users.noreply.github.com>
2025-10-19 22:01:46 +0300

    Updated AdminRole.java

--- src/AdminRole.java
+++ src/AdminRole.java
@@ -18,16 +18,17 @@ public class AdminRole {
     }
     EmployeeUser newUser = new EmployeeUser(employeeId, name, email, address, phoneNumber);
     database.insertRecord(newUser);
-}
+    }
 
     public EmployeeUser[] getListOfEmployees() {
         return database.returnAllRecords().toArray(new EmployeeUser[0]);
     }
 
+    
     public void removeEmployee(String key) {
         database.deleteRecord(key);
     }
-
+    
     public void logout() {
         database.saveToFile();
     }
